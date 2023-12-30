3a036d9f2cd5002c6a5cf50d4403911cab55ee8b
hijunglit <hijunglit@gmail.com>
2023-12-31 01:11:19 +0900

    git push (middleware & morgan)

--- src/server.js
+++ src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
 
+const logger = morgan("tiny");
+
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
-app.get("/", (req, res) => {
-  res.send("I still love you");
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
+
+app.use(logger);
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+app.get("/", handleHome);
 
 app.listen(PORT, handleListening);

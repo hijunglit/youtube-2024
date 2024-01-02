1d17bfd799cf79763bbcaaa652702cb4c4a359ab
hijunglit <hijunglit@gmail.com>
2024-01-02 13:40:40 +0900

    use pug

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,9 +1,12 @@
-export const trending = (req, res) => res.render("home");
-export const watch = (req, res) => res.send("Watch");
-export const see = (req, res) => {
-  return res.send(`watch video #${req.params.id}`);
-};
-export const edit = (req, res) => res.send("Edit");
+const fakeUser = {
+  username: "tomas",
+  loggedIn: true,
+}
+
+export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
+export const see = (req, res) => res.render("watch", {pageTitle: "watch"});
+export const watch = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit", {pageTitle: "edit"});
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {

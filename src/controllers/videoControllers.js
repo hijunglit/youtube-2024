ff0548ed45f7982704fdb0fe18c7a91e12d0382d
hijunglit <hijunglit@gmail.com>
2024-01-02 14:39:44 +0900

    working on pug & push

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,9 +1,20 @@
-const fakeUser = {
-  username: "tomas",
-  loggedIn: true,
-}
-
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+  const fakeUser = {
+    username: "tomas",
+    loggedIn: false,
+  }
+  const videos = [
+    {
+      title: "title",
+      rating: "5.0",
+      comments: "great",
+      createdAt: "2024.1.2",
+      views: 1,
+    }
+  ];
+  // const videos = null;
+  res.render("home", { pageTitle: "Home", fakeUser, videos })
+};
 export const see = (req, res) => res.render("watch", {pageTitle: "watch"});
 export const watch = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit", {pageTitle: "edit"});

f00cc4189fedde23f1663f106e834fe3f49b986e
hijunglit <hijunglit@gmail.com>
2024-01-07 19:40:29 +0900

    mongoose queries with promise

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
-import db from "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 const logger = morgan("tiny");
@@ -18,7 +14,4 @@ app.use("/", globalRouter);
 app.use("/video", videoRouter);
 app.use("/user", userRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;

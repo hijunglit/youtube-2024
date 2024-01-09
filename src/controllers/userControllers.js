bf042b71d98e67c33a9802559fef0d0b95c658e9
hijunglit <hijunglit@gmail.com>
2024-01-09 17:40:49 +0900

    form validation

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,7 +1,30 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {
-  console.log(req.body);
-  res.end();
+export const postJoin = async (req, res) => {
+  const { name, email, username, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  const exists = await User.exists({ $or: [{ email }, { username }] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "email/username is already taken",
+    });
+  }
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "password comfirm does not match",
+    });
+  }
+  await User.create({
+    name,
+    email,
+    username,
+    password,
+    location,
+  });
+  return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("edit");
 export const remove = (req, res) => res.send("remote user");

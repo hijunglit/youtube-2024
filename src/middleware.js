15b8fb16e4178f3cd9c4245c5da3870765527ad5
hijunglit <hijunglit@gmail.com>
2024-01-10 14:59:14 +0900

    push

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,6 @@
 export const localMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
-    res.locals.user = req.session.user;
+    res.locals.loggedInUser = req.session.user;
     console.log(res.locals);
     next();
 }

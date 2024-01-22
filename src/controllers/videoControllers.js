3be5fb7c83f477631513fdb0ed533a93f785aef7
hijunglit <hijunglit@gmail.com>
2024-01-22 23:27:03 +0900

    post without browser

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -115,8 +115,9 @@ export const search = async (req, res) => {
 export const registerView = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
+  console.log(video);
   if (!video) {
-    return res.status(404);
+    return res.sendStatus(404);
   }
   video.meta.views = video.meta.views + 1;
   await video.save();

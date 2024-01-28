8e594eacb7dbf84c60cf177bbb7c8342c0624eea
hijunglit <hijunglit@gmail.com>
2024-01-28 23:56:01 +0900

    realtime comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,24 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.querySelector("form");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa__comment";
+  const span = document.createElement("span");
+  span.innerText = text;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
   const videoId = videoContainer.dataset.id;
-  await fetch(`/api/video/${videoId}/comment`, {
+  const response = await fetch(`/api/video/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -15,7 +27,12 @@ const handleSubmit = async (event) => {
       text,
     }),
   });
-  textarea.value = "";
+  if (response.status === 201) {
+    console.log(response);
+    console.log("add fake comment! g");
+    addComment(text);
+    textarea.value = "";
+  }
 };
 
 if (form) {

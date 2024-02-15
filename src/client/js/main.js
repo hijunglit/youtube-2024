66f244c05ff60b4bb5b7897a4b36afbe01c4701d
hijunglit <hijunglit@gmail.com>
2024-02-15 17:34:59 +0900

    side menu

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,1 +1,11 @@
 import "../scss/styles.scss";
+
+const menuIcon = document.querySelector(".menu-bar__icon");
+const menuBar = document.querySelector(".menu-bar");
+const spreadBar = document.querySelector(".spread-bar");
+
+function handleSideMenu() {
+  menuBar.classList.toggle("close");
+  spreadBar.classList.toggle("close");
+}
+menuIcon.addEventListener("click", handleSideMenu);

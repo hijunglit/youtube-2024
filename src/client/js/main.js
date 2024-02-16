deb0adb8803e8baecbd39783c7e1b6ea59b932aa
hijunglit <hijunglit@gmail.com>
2024-02-16 11:45:59 +0900

    user menu popup

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -4,8 +4,15 @@ const menuIcon = document.querySelector(".menu-bar__icon");
 const menuBar = document.querySelector(".menu-bar");
 const spreadBar = document.querySelector(".spread-bar");
 
+const userMenuIcon = document.querySelector(".user__menu");
+const userPopup = document.querySelector(".user__popup-container");
+
 function handleSideMenu() {
   menuBar.classList.toggle("close");
   spreadBar.classList.toggle("close");
 }
+function handleUserPopup() {
+  userPopup.classList.toggle("off");
+}
 menuIcon.addEventListener("click", handleSideMenu);
+userMenuIcon.addEventListener("click", handleUserPopup);
